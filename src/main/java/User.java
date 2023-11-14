8124daa291d5ea0dd422091a3140f30984643ff2
Marwan Abdalla <marwan3m2000@gmail.com>
2023-11-14 19:06:50 +0100

    user can be added to userlist

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,8 +3,10 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -16,6 +18,7 @@ public class User {
     private String username;
     private String password;
     private List<User> friendlist;
+    public static Set<User> userlist = new HashSet<>();
 
     public User(String firstname, String lastname, String username, String password) {
         this.firstname = firstname;
@@ -23,6 +26,10 @@ public class User {
         this.username = username;
         this.password = password;
         this.friendlist = new LinkedList<>();
+        userlist.add(this);
+    }
+    public static void printUserList() {
+        System.out.println(userlist);
     }
 
 }
