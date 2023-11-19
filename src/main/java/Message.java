1154f7a8f868240a10deaec962138ce5a9fbd6ee
fav0u <90066018+fav0u@users.noreply.github.com>
2023-11-19 21:03:26 +0100

    applied proposed changes

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,19 +1,22 @@
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode
 public class Message {
-    private String content;
+    private String data;
     private LocalDateTime time;
     private User sender;
 
-    public Message(String content, LocalDateTime time, User sender) {
-        this.content = content;
+    public Message(String data, LocalDateTime time, User sender) {
+        this.data = data;
         this.time = time;
         this.sender = sender;
     }
