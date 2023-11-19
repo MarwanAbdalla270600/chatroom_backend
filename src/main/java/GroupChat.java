58fbe3cb6f53b7f18837db2df7c6c57d24364a67
fav0u <90066018+fav0u@users.noreply.github.com>
2023-11-19 21:02:20 +0100

    applied proposed changes

--- src/main/java/GroupChat.java
+++ src/main/java/GroupChat.java
@@ -1,21 +1,25 @@
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.LinkedList;
 import java.util.List;
+
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode(callSuper = true)
 public class GroupChat extends Chat{
     private String groupName;
-    private List<User> participants;
-    private int maxParticipants;
+    private List<User> members;
+    private int maxMembers;
 
-    public GroupChat(String chatID, LocalDateTime time, List<Message> messageList, String groupName, List<User> participants, int maxParticipants) {
-        super(chatID, time, messageList);
+    public GroupChat(String chatID, LocalDateTime time, String groupName, List<User> members, int maxMembers) {
+        super(chatID, time, new LinkedList<>());
         this.groupName = groupName;
-        this.participants = participants;
-        this.maxParticipants = maxParticipants;
+        this.members = members;
+        this.maxMembers = maxMembers;
     }
 }
