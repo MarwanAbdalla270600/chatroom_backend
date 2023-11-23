08ce32618c2279b0d18a8093a1b607393e9b6704
Philipp Hoertnagl <ic22b116@technikum-wien.at>
2023-11-23 12:01:12 +0100

    use of EqualsAndHashCode.Include for messageId field

--- src/main/java/message/Message.java
+++ src/main/java/message/Message.java
@@ -3,33 +3,22 @@ package message;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.EqualsAndHashCode;
 
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public abstract class Message {
+    @EqualsAndHashCode.Include
     private int messageId;
     private LocalDateTime time;
 
     public Message(int messageId) {
         this.messageId = messageId;
         this.time = LocalDateTime.now();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Message message = (Message) o;
-        return messageId == message.messageId;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(messageId);
     }
 }
