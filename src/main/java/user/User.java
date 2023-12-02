0d6e33e8a5ba3451891db034e0487dbd876dc3cd
Philipp Hoertnagl <ic22b116@technikum-wien.at>
2023-12-02 23:41:36 +0100

    added sendFriendRequest() implementation

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -6,9 +6,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.EqualsAndHashCode;
+import message.FriendRequestMessage;
+import message.Message;
+import message.StatusMessage;
 
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 
@@ -24,6 +27,7 @@ public class User {
     private Set<User> friendList;
     private Set<GroupChat> groupChats;
     private Set<PrivateChat> privateChats;
+    private ArrayList<FriendRequest> friendRequests;
 
     public User( String username, String password) {
         this.username = username;
@@ -31,6 +35,28 @@ public class User {
         this.friendList = new HashSet<>();
         this.groupChats = new HashSet<>();
         this.privateChats = new HashSet<>();
+        this.friendRequests = new ArrayList<>();
     }
+
+    public void sendFriendRequest(User receiver) {
+        if(receiver != null && !this.friendList.contains(receiver) && !hasSentRequest(receiver)) {
+            FriendRequestMessage friendRequestMessage = new FriendRequestMessage(this, receiver);
+            FriendRequest friendRequest = new FriendRequest(this, receiver);
+            this.friendRequests.add(friendRequest);
+            receiver.friendRequests.add(friendRequest);
+            StatusMessage statusMessage = new StatusMessage("Friend request sent to " + receiver.getUsername());
+
+        }
+    }
+
+    public boolean hasSentRequest(User receiver) {
+        for (FriendRequest request : friendRequests) {
+            if (request.getReceiver().equals(receiver) && request.getStatus() == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
