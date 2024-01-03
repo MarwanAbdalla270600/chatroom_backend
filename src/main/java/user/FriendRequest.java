4ab16671c8b49029dad5af7d39b6723831257f76
phl <ic22b116@technikum-wien.at>
2024-01-03 18:52:33 +0100

    added toString() method

--- src/main/java/user/FriendRequest.java
+++ src/main/java/user/FriendRequest.java
@@ -2,9 +2,11 @@ package user;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+
 public class FriendRequest {
     private User sender;
     private User receiver;
@@ -20,6 +22,11 @@ public class FriendRequest {
         this.sender = sender;
         this.receiver = receiver;
         this.status = FriendRequestStatus.PENDING;
+    }
+
+    @Override
+    public String toString() {
+        return "FriendRequest from " + sender.getUsername() + " to " + receiver.getUsername() + " - Status: " + status;
     }
 
 }
