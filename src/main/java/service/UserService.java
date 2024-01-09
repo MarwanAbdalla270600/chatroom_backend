789049fcb5e885d2b3de05f66b563a5d8a900bc9
Philipp Hoertnagl <ic22b116@technikum-wien.at>
2024-01-09 13:06:05 +0100

    added registerNewUser(), changePassword(), changeUsername() methods

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -21,22 +21,55 @@ public class UserService {
             return false;
         }
 
-        if (isValidPassword(password)) {
+        if (!isValidPassword(password)) {
             System.out.println("Password invalid. Passwords must be between 6 and 25 characters long and must contain " +
                     "numbers AND digits!");
             return false;
         }
-        //denk für gender wirds keine Validierung brauchen, weil man im GUI graphisch nur zwischen weibl./männl. wählen können soll?!
+        //denk für gender wirds keine Validierung brauchen, weil man im GUI graphisch nur zwischen weibl./männl. wählt?!
 
         if (!registeredUsers.containsKey(username)) {
             User newUser = new User(username, password, gender);
             registeredUsers.put(username, newUser);
+            System.out.println("New User " + newUser.getUsername() + "successfully registered");
             return true;
         }
 
         System.out.println("Username already exists, please choose a other name");
         return false;
     }
+
+    public boolean changeUsername(String oldUsername, String newUsername) { //method if a User wants to change username later on
+        if (!isValidUsername(newUsername)) {
+            System.out.println("New Username invalid.");
+            return false;
+        }
+        if (registeredUsers.containsKey(oldUsername) && !registeredUsers.containsKey(newUsername)) { //check ob alter username existiert und neuer noch frei ist
+            User user = registeredUsers.get(oldUsername);
+            user.setUsername(newUsername);
+            registeredUsers.remove(oldUsername);    //update hashmap with new key
+            registeredUsers.put(newUsername, user);
+            System.out.println("Username successfully changed from " + oldUsername + "to " + user.getUsername());
+            return true;
+        }
+        System.out.println("Old Username does not exist or new Username is not free (Username already exists");
+        return false;
+    }
+
+    public boolean changePassword(String username, String newPassword) {
+        if (!isValidPassword(newPassword)) {
+            System.out.println("New Password is not valid.");
+            return false;
+        }
+        if (registeredUsers.containsKey(username)) {
+            User user = registeredUsers.get(username);
+            user.setPassword(newPassword);
+            System.out.println("Password successfully changed");
+            return true;
+        }
+        return false;
+    }
+
 
 
     private static boolean isValidUsername(String username) {
@@ -55,7 +88,7 @@ public class UserService {
         return true;
     }
 
-    private static boolean isValidPassword(String password) {
+    private static boolean isValidPassword(String password) { //checks that pw's have digits AND letters and 6-25 char length
         if (password == null) return false;
 
         int minLength = 6;
